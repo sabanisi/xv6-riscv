1f826a3c7438d3faf518b29b1c89985c62187b14
sabanisi <taisuke140909@gmail.com>
2023-11-05 22:22:29 +0900

    Update pingpong.c

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,6 +1,6 @@
 // pingpong.c for xv6 (assignment #1 for CSC.T371)
-// name: <<<your name>>>
-// id: <<<your student id>>>
+// name: Taisuke Sakugawa
+// id: 21B30362
 
 #include "kernel/types.h"
 #include "kernel/stat.h"
@@ -20,12 +20,48 @@ int main(int argc, char *argv[]) {
     // tick value before starting rounds
     int start_tick = uptime();
 
-    // <<<remove this comment and fill your code here>>>
+    int pp[2],qq[2];
+    unsigned char buf[1];
+    unsigned int num =0;
 
-    // tick value after ending rounds
-    int end_tick = uptime();
-    // print # of ticks in N rounds
-    printf("# of ticks in %d rounds: %d\n", n, end_tick - start_tick);
+    //create pipe
+    pipe(pp); //child -> parent
+    pipe(qq); //parent -> child
+
+    int status;
+    if(fork() >  0){
+        //parent proces
+        close(qq[0]);
+        close(pp[1]);
+
+        for(int i=0;i<n;i++){
+            buf[0]=(unsigned char)num%256;
+            write(qq[1],buf,1);
+            read(pp[0],buf,1);
+            num = (int)buf[0];
+            num++;
+        }
+
+        //wait child process
+        wait(&status);
+
+        // tick value after ending rounds
+        int end_tick = uptime();
+        // print # of ticks in N rounds
+        printf("# of ticks in %d rounds: %d\n", n, end_tick - start_tick);
+        exit(0);
+    }else{
+        //child process
+        close(pp[0]);
+        close(qq[1]);
+
+        for(int i=0;i<n;i++){
+            read(qq[0],buf,1);
+            num = (int)buf[0];
+            num++;
+            buf[0]=(unsigned char)num%256;
+            write(pp[1],buf,1);
+        }
+    }
     exit(0);
 }
-
